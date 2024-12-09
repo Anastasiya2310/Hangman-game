ea0c0a3d6559dd1ce87b5ccb0548f012dc2c1111
Anastasiya Kharitonova <anastasiya.kovalskaya@warecorp.com>
2024-12-09 16:15:18 +0100

    test for modal component

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -12,14 +12,16 @@ const Modal = ({
   show, 
   gameOver, 
   winner }) => {
-  const showHideClassModal = show ? 'modal visible-modal' : 'modal hidden-modal';
   const handleStartGame = () => {
     hideModal();
   };
 
   return (
-    <div className={`${show ? 'modal-wraper visible' : 'modal-wraper hidden'} ${gameOver ? 'darkenBg' : 'transparentBg'}`}>
-      <div className={showHideClassModal}>
+    <div 
+      role="dialog"
+      aria-modal="true"
+      className={`modal-wraper ${show ? 'visible' : 'hidden'} ${gameOver ? 'darkenBg' : 'transparentBg'}`}>
+      <div className='modal'>
         {gameOver ? (<h1 className="title title-fail">Game Over</h1>) : null}
         {winner ? (<><h1 className="title title-success">You win! Congratulations!</h1><Lottie className="win-animation" animationData={animationData} style={{ width: 200, height: 200 }}></Lottie></>) : null}
         <h2>Choose the category of questions</h2>
